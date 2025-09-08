80c71677a5b3bc4d49071737b92aa568b783325b
AnaClaudiaOT <ana.claudia.tavares@hotmail.com>
2025-09-08 20:58:26 -0300

    Correcao de bug

--- services/transferService.js
+++ services/transferService.js
@@ -4,7 +4,7 @@ const transferModel = require("../models/transferModel");
 function transfer(from, to, value) {
   const sender = userModel.findByUsername(from);
   const recipient = userModel.findByUsername(to);
-  if (!sender || !recipient) return { error: "Usuário não existe" };
+  if (!sender || !recipient) return { error: "Usuário não encontrado" };
   if (sender.saldo < value) return { error: "Saldo insuficiente" };
   if (!recipient.favorecido && value >= 5000) {
     return { error: "Transferência acima de R$ 5.000,00 só para favorecidos" };
